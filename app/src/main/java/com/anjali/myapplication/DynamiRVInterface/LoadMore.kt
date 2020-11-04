393a0bf779ef9a9b1b19a2206a6ade5b850aef85
Anjali <anjalikumari13617@gmail.com>
2020-11-04 13:38:38 +0530

    merged both projects

--- app/src/main/java/com/anjali/myapplication/DynamiRVInterface/LoadMore.kt
+++
@@ -1,6 +0,0 @@
-package com.anjali.myapplication.DynamiRVInterface
-
-interface LoadMore {
-
-    fun onLoadMore(){}
-}
--- app/src/main/java/com/anjali/myapplication/models/DynamicRVModelClass.kt
+++
@@ -1,9 +0,0 @@
-package com.anjali.myapplication.models
-
-class DynamicRVModelClass(val name:String) {
-
-    @JvmName("getName1")
-    fun getName():String{
-        return name
-    }
-}
